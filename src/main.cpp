c3aea09d35d360e52139e64f498f885ae07ee9bd
Damian Wojsław <dwojslaw@redhat.com>
2020-10-15 15:58:15 +0200

    Start main

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,17 @@
 // Created by trochej on 10/15/20.
 //
 
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main(int argc, char *argv[]){
+    using namespace boost;
+    using ip::tcp;
+    using std::string;
+    using std::cout;
+    using std::endl;
+
+
+
+    return 0;
+}
