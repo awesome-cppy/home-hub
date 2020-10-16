d819b4f2c532230d9dc77ef54a50f8ff3e7d0b64
Damian Wojsław <dwojslaw@redhat.com>
2020-10-16 17:19:15 +0200

    Set for testing

---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by trochej on 10/16/20.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
