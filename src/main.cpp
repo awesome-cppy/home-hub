f019c701aa5ad8cd46ce55027f31fc4d84d21be5
Damian Wojsław <dwojslaw@redhat.com>
2020-10-28 21:18:25 +0100

    Stub of server

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,28 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
+#include <memory>
+
+using namespace boost;
+
+std::string readFromSocket(asio::ip::tcp::socket &socket){
+    asio::streambuf buf;
+    asio::read_until(socket, buf, "\n");
+    std::string data = asio::buffer_cast<const char*>(buf.data());
+    return data;
+}
+
+void writeToSocket(asio::ip::tcp::socket& socket, const std::string& message){
+    const std::string msg = message + "\n";
+    asio::write(socket, asio::buffer(message));
+}
 
 int main(int argc, char *argv[]){
-    using namespace boost;
-    using ip::tcp;
-    using std::string;
-    using std::cout;
-    using std::endl;
-
-
-
+    asio::io_service iosrvc;
+    asio::ip::tcp::acceptor acceptor(iosrvc, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 2048));
+    asio::ip::tcp::socket socket(iosrvc);
+    acceptor.accept(socket);
+    std::string message = readFromSocket(socket);
+    writeToSocket(socket, "ACK");
     return 0;
 }
